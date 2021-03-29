f9f71e34f79d4207627d00dbfbdcd6d2f02533c2
Faryne <faryne@gmail.com>
2021-03-29 23:45:15 +0800

    fix js

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,11 +1,15 @@
 let df = require("dateformat");
 export default {
     returnLocalDate($str){
+        if ($str == 'undefined' || $str == null) {
+            return '-';
+        }
         let newStr = $str.replace(" ", "T");
         if (newStr.substring(newStr.length - 1) != 'Z') {
             newStr += 'Z';
         }
+        let $d = new Date(newStr);
 
-        return df(new Date(newStr), 'yyyy-mm-dd HH:MM:ss') + " " + Intl.DateTimeFormat().resolvedOptions().timeZone;
+        return df($d, 'yyyy-mm-dd HH:MM:ss') + " " + Intl.DateTimeFormat().resolvedOptions().timeZone;
     }
 }
